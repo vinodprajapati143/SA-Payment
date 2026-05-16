a6d13668e9301aa3b84a469e00da536eb5dabec6
Vinod Prajapati <78250670+vinodprajapati143@users.noreply.github.com>
2026-05-16 21:45:57 +0530

    fix cahnges

--- AK-BOSS_FE/src/app/payment/about/about.component.ts
+++ AK-BOSS_FE/src/app/payment/about/about.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { SettingService } from '../../core/services/setting.service';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  private settingStore = inject(SettingService);
+  sitename: any;
+  copyright: any;
 
+  ngOnInit(): void {
+        this.settingStore.getSite().subscribe(res => {
+      if (res) {
+        this.sitename = res.name || 'AK-BOSS';
+        this.copyright = res.copyright || '';
+      } });
+  }
 }
